dfcdec690eb0babcb9203a06ba58681b57785182
= <=>
2022-08-19 17:40:28 -0300

    Mapeando a entidade cliente JPA

--- src/main/java/io/github/gabriellmelo10/domain/entity/Cliente.java
+++ src/main/java/io/github/gabriellmelo10/domain/entity/Cliente.java
@@ -1,9 +1,30 @@
 package io.github.gabriellmelo10.domain.entity;
 
+import javax.persistence.*;
+
+@Entity
+@Table( name = "cliente" )
 public class Cliente {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id")
     private Integer id;
+
+    @Column(name = "nome", length = 100)
     private String nome;
+
+    public Cliente() {
+    }
+
+    public Cliente(Integer id, String nome) {
+        this.id = id;
+        this.nome = nome;
+    }
+
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
 
     public Integer getId() {
         return id;
@@ -19,5 +40,13 @@ public class Cliente {
 
     public void setNome(String nome) {
         this.nome = nome;
+    }
+
+    @Override
+    public String toString() {
+        return "Cliente{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                '}';
     }
 }
