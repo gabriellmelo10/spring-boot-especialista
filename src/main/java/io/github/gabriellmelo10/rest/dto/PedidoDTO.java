6bbee7a2a6a092eee1722b6b6ad3349300eb6a11
= <=>
2022-08-25 21:33:00 -0300

    Criando annotation customizada

--- src/main/java/io/github/gabriellmelo10/rest/dto/PedidoDTO.java
+++ src/main/java/io/github/gabriellmelo10/rest/dto/PedidoDTO.java
@@ -1,9 +1,11 @@
 package io.github.gabriellmelo10.rest.dto;
 
+import io.github.gabriellmelo10.validation.NotEmptyList;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotNull;
 import java.math.BigDecimal;
 import java.util.List;
 
@@ -11,7 +13,14 @@ import java.util.List;
 @AllArgsConstructor
 @NoArgsConstructor
 public class PedidoDTO {
+
+    @NotNull(message = "Informe o código do cliente.")
     private Integer cliente;
+
+    @NotNull(message = "Campo Total do pedido é obrigatório.")
     private BigDecimal total;
+
+    @NotEmptyList(message = "Pedido não pode ser realizado sem itens.")
     private List<ItemPedidoDTO> items;
+
 }
