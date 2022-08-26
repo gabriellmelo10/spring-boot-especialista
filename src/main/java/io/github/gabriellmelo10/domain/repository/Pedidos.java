765113bdb0594360eaf2230adf0eb19932473a2d
= <=>
2022-08-25 21:50:58 -0300

    Internacionalização

--- src/main/java/io/github/gabriellmelo10/domain/repository/Pedidos.java
+++ src/main/java/io/github/gabriellmelo10/domain/repository/Pedidos.java
@@ -3,11 +3,17 @@ package io.github.gabriellmelo10.domain.repository;
 import io.github.gabriellmelo10.domain.entity.Cliente;
 import io.github.gabriellmelo10.domain.entity.Pedido;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 import java.util.Set;
 
 public interface Pedidos extends JpaRepository<Pedido, Integer> {
 
     List<Pedido> findByCliente(Cliente cliente);
+
+    @Query(" select p from Pedido p left join fetch p.itens where p.id = :id ")
+    Optional<Pedido> findByIdFetchItens(@Param("id") Integer id);
 }
--- src/main/java/io/github/gabriellmelo10/service/PedidoService.java
+++ src/main/java/io/github/gabriellmelo10/service/PedidoService.java
@@ -1,8 +1,13 @@
 package io.github.gabriellmelo10.service;
 
 import io.github.gabriellmelo10.domain.entity.Pedido;
+import io.github.gabriellmelo10.domain.enums.StatusPedido;
 import io.github.gabriellmelo10.rest.dto.PedidoDTO;
+
+import java.util.Optional;
 
 public interface PedidoService {
     Pedido salvar( PedidoDTO dto );
+    Optional<Pedido> obterPedidoCompleto(Integer id);
+    void atualizaStatus(Integer id, StatusPedido statusPedido);
 }
