e339eb12e093321732f14c5c19c442b03db32ba5
= <=>
2022-09-26 19:09:58 -0300

    Testando a autenticação com cadastro de usuários

---
+++ src/main/java/io/github/gabriellmelo10/domain/repository/UsuarioRepository.java
@@ -0,0 +1,11 @@
+package io.github.gabriellmelo10.domain.repository;
+
+import io.github.gabriellmelo10.domain.entity.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+
+    Optional<Usuario> findByLogin(String login);
+}
