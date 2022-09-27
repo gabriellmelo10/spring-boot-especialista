a6d1c835b28c34be2c53502d130703787f67a066
= <=>
2022-09-26 21:37:16 -0300

    Implementação Token jwt

--- src/main/java/io/github/gabriellmelo10/config/SecurityConfig.java
+++ src/main/java/io/github/gabriellmelo10/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package io.github.gabriellmelo10.config;
 
+import io.github.gabriellmelo10.security.jwt.JwtAuthFilter;
+import io.github.gabriellmelo10.security.jwt.JwtService;
 import io.github.gabriellmelo10.service.impl.UsuarioServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -8,18 +10,28 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.filter.OncePerRequestFilter;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     private UsuarioServiceImpl usuarioService;
+    @Autowired
+    private JwtService jwtService;
 
     @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public OncePerRequestFilter jwtFilter(){
+        return new JwtAuthFilter(jwtService, usuarioService);
     }
 
     @Override
@@ -41,10 +53,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .antMatchers("/api/produtos/**")
                     .hasRole("ADMIN")
                 .antMatchers(HttpMethod.POST, "/api/usuarios/**")
-                    .permitAll() //qualquer pessoa pode fazer um cadastro de usuario
+                    .permitAll()
                 .anyRequest().authenticated()
             .and()
-                .httpBasic();
+                .sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            .and()
+                .addFilterBefore( jwtFilter(), UsernamePasswordAuthenticationFilter.class);
         ;
     }
 
