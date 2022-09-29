c82ef871bbb25e7e606b50f46bfadce3ede86e4b
= <=>
2022-09-28 22:45:58 -0300

    swagger

---
+++ src/main/java/io/github/gabriellmelo10/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package io.github.gabriellmelo10.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    @Bean
+    public Docket docket(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
+                .select()
+                .apis(RequestHandlerSelectors
+                        .basePackage("io.github.gabriellmelo10.rest.controller"))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo(){
+        return new ApiInfoBuilder()
+                .title("Vendas API")
+                .description("Api do projeto de Vendas")
+                .version("1.0")
+                .contact(contact())
+                .build();
+    }
+
+    private Contact contact(){
+        return new Contact("Gabriel Melo"
+                , "http://github.com/gabriellmelo10",
+                "gabriellmelo10@gmail.com");
+    }
+
+
+}
